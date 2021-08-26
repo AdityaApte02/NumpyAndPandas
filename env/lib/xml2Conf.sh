7eb41aae4fadb2b44888eca21d984c02e1beaea5
aditya <aditya02.apte@gmail.com>
2021-08-26 14:24:39 +0530

    First Commit

---
+++ env/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/aditya/Documents/Machine_Learning/sample_project/env/lib"
+XML2_LIBS="-lxml2 -L/home/aditya/Documents/Machine_Learning/sample_project/env/lib -lz -L/home/aditya/Documents/Machine_Learning/sample_project/env/lib -llzma -lpthread -L/home/aditya/Documents/Machine_Learning/sample_project/env/lib -L/home/aditya/Documents/Machine_Learning/sample_project/env/lib -licui18n -licuuc -licudata -lm "
+XML2_INCLUDEDIR="-I/home/aditya/Documents/Machine_Learning/sample_project/env/include/libxml2 -I/home/aditya/Documents/Machine_Learning/sample_project/env/include"
+MODULE_VERSION="xml2-2.9.10"
+
